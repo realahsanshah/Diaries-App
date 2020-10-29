e3a9a79f810475ef89c5384d4db4b57eee7e66de
Ahsan Shah <56998278+realsashah@users.noreply.github.com>
2020-10-29 15:23:41 +0500

    Added store and authSlice

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,1 +1,13 @@
 import {combineReducers} from '@reduxjs/toolkit';
+import authReducer from '../features/auth/authSlice';
+import userReducer from '../features/auth/userSlice';
+
+
+const rootReducer=combineReducers({
+    auth:authReducer,
+    user:userReducer,
+});
+
+export type RootState=ReturnType<typeof rootReducer>;
+
+export default rootReducer;
