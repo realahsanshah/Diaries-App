52b916095f7f5fa9d3ed88230a1a0fef7e81feef
Ahsan Shah <56998278+realsashah@users.noreply.github.com>
2020-10-29 17:41:13 +0500

    Added components

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import Diaries from '../diary/Diaries';
+import Editor from '../entry/Editor';
 
-
-const Home= () => {
+const Home = () => {
     return (
-        <div>Home</div>
+        <div className="two-cols">
+            <div className="left">
+                <Diaries />
+            </div>
+            <div className="right">
+                <Editor />
+            </div>
+        </div>
     );
 }
- 
+
 export default Home;
