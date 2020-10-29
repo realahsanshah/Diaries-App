872f97420d96f6eb43784d01e66051bd6ad71958
Ahsan Shah <56998278+realsashah@users.noreply.github.com>
2020-10-29 16:16:32 +0500

    Provided store

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {setupServer} from './services/mirage/server';
+import { setupServer } from './services/mirage/server';
+import { Provider } from 'react-redux';
+import store from './store/store';
 
-if(process.env.NODE_ENV==='development'){
+
+if (process.env.NODE_ENV === 'development') {
   setupServer();
 }
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,4 +1,4 @@
-import {combineReducers} from '@reduxjs/toolkit';
+import { combineReducers } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
 import userReducer from '../features/auth/userSlice';
 import diariesReducer from '../features/diary/diarySlice';
@@ -6,14 +6,14 @@ import entriesReducer from '../features/entry/entriesSlice';
 import editorReducer from '../features/entry/editorSlice';
 
 
-const rootReducer=combineReducers({
-    auth:authReducer,
-    user:userReducer,
-    entries:entriesReducer,
-    diaries:diariesReducer,
-    editor:editorReducer,
+const rootReducer = combineReducers({
+    auth: authReducer,
+    user: userReducer,
+    entries: entriesReducer,
+    diaries: diariesReducer,
+    editor: editorReducer,
 });
 
-export type RootState=ReturnType<typeof rootReducer>;
+export type RootState = ReturnType<typeof rootReducer>;
 
 export default rootReducer;
