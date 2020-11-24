88df20a1aafb2199c7af21c8fe06e94d0a3bdf9c
Ahsan Shah <56998278+realsashah@users.noreply.github.com>
2020-11-25 03:00:02 +0500

    removed env

--- src/index.tsx
+++ src/index.tsx
@@ -7,9 +7,12 @@ import { setupServer } from './services/mirage/server';
 import { Provider } from 'react-redux';
 import store from './store';
 
-if (process.env.NODE_ENV === 'development') {
-  setupServer();
-}
+// if (process.env.NODE_ENV === 'development') {
+//   setupServer();
+// }
+
+setupServer();
+
 
 ReactDOM.render(
   <React.StrictMode>
